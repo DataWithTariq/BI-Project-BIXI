8a9ca0274b853b9b5f5ee177ddd2042a4089115b
DataWithTariq <tariqladidji43@gmail.com>
2025-06-30 00:38:06 -0400

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -1,3 +1,14 @@
+/*
+===============================================================================
+DDL Script: Create Silver Tables
+===============================================================================
+Script Purpose:
+    This script creates tables in the 'silver' schema, dropping existing tables 
+    if they already exist.
+	  Run this script to re-define the DDL structure of 'bronze' Tables
+===============================================================================
+*/
+
 IF OBJECT_ID ('silver.trips_2021','U') IS NOT NULL
     DROP TABLE silver.trips_2021;
 GO
